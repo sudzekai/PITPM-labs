f041c9568c1c233e4411d3d37d4825a88511f517
sudzekai <yomakum@mail.ru>
2025-12-14 22:23:51 +0300

    10 lw

---
+++ LabWork10_Library/PasswordValidator.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace LabWork10_Library
+{
+    public static partial class PasswordValidator
+    {
+        [GeneratedRegex(@"^(?=.*\d)(?=.*[a-zA-Z]).{8,}$")]
+        private static partial Regex PasswordRegex();
+
+        public static bool IsValidPassword(this string password)
+            => PasswordRegex().IsMatch(password);
+    }
+}
