813e58ce98e62a0b0d0e3be032a3174a1074249c
sudzekai <yomakum@mail.ru>
2025-12-15 04:28:12 +0300

    lw 8 init

---
+++ LW8_Test/MainWindowTester.cs
@@ -0,0 +1,86 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+
+namespace LW8_Test
+{
+    public class MainWindowTester
+    {
+        private Application _app;
+        private UIA3Automation _automation;
+
+        public MainWindowTester()
+        {
+            _app = Application.Launch(@"C:\Users\yomak\source\repos\IntegrationTesting\LabWork8\bin\Debug\net10.0-windows\LabWork8.exe");
+            _automation = new UIA3Automation();
+
+        }
+
+        [Fact]
+        public void Test_WrongLogin_Exception()
+        {
+            DoTest("123", "admin", nameof(Test_WrongLogin_Exception));
+        }
+
+        [Fact]
+        public void Test_WrongPassword_Exception()
+        {
+            DoTest("admin", "123", nameof(Test_WrongPassword_Exception));
+        }
+
+        [Fact]
+        public void Test_WrongLoginAndPassword_Exception()
+        {
+            DoTest("123", "123", nameof(Test_WrongLoginAndPassword_Exception));
+        }
+
+        [Fact]
+        public void Test_WhiteSpaceLogin_Exception()
+        {
+            DoTest("", "admin", nameof(Test_WhiteSpaceLogin_Exception));
+        }
+
+        [Fact]
+        public void Test_WhiteSpacePassword_Exception()
+        {
+            DoTest("admin", "", nameof(Test_WhiteSpacePassword_Exception));
+        }
+
+        [Fact]
+        public void Test_TrueLoginAndPassword_Good()
+        {
+            DoTest("admin", "admin", nameof(Test_TrueLoginAndPassword_Good), "Успех");
+        }
+
+        public void TakeScreenshot(FlaUI.Core.AutomationElements.Window window, string name, string error = "")
+        {
+            var screenshot = window.Capture();
+         
+            if (!Directory.Exists($"screenshots/{name}"))
+                Directory.CreateDirectory($"screenshots/{name}");
+
+            screenshot.Save($"screenshots/{name}/{error}-screenshot.png");
+        }
+
+        private void DoTest(string login, string password, string funcName, string errorWindowName = "Ошибка")
+        {
+            var window = _app.GetMainWindow(_automation);
+            var loginInput = window.FindFirstDescendant(cf => cf.ByAutomationId("LoginTextBox")).AsTextBox();
+            var passwordInput = window.FindFirstDescendant(cf => cf.ByAutomationId("PasswordTextBox")).AsTextBox();
+            var button = window.FindFirstDescendant(cf => cf.ByAutomationId("AuthButton")).AsButton();
+
+            passwordInput.Text = password;
+            loginInput.Text = login;
+            button.Invoke();
+
+            var errorWindow = _automation.GetDesktop().FindFirstDescendant(cf => cf.ByName(errorWindowName)).AsWindow();
+            TakeScreenshot(errorWindow, funcName, "error-");
+            errorWindow.Close();
+            TakeScreenshot(window, funcName);
+
+            Assert.NotNull(errorWindow);
+            _app.Close();
+            _automation.Dispose();
+        }
+    }
+}
---
+++ LabWork8/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace LabWork8
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Visibility _authVisibility = Visibility.Visible;
+        public Visibility AuthVisibility
+        {
+            get => _authVisibility;
+            set
+            {
+                if (_authVisibility == value) return;
+                _authVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _login = "";
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                if (_login == value) return;
+                _login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _password = "";
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password == value) return;
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RelayCommand _authCommand;
+        public RelayCommand AuthCommand
+        {
+            get => _authCommand ??= new RelayCommand((o) =>
+            {
+                if (string.IsNullOrWhiteSpace(Login))
+                    MessageBox.Show("Поле логин обязательно для ввода", "Ошибка");
+                else if (string.IsNullOrWhiteSpace(Password))
+                    MessageBox.Show("Поле пароль обязательно для ввода", "Ошибка");
+
+                else if (!Login.Equals("admin") && !Password.Equals("admin"))
+                    MessageBox.Show("Логин и пароль введены неверно", "Ошибка");
+
+                else if (!Login.Equals("admin"))
+                    MessageBox.Show("Логин введён неверно", "Ошибка");
+                else if (!Password.Equals("admin"))
+                    MessageBox.Show("Пароль введён неверно", "Ошибка");
+                else
+                {
+                    MessageBox.Show($"С возвращением {Login}", "Успех");
+                    AuthVisibility = Visibility.Collapsed;
+                }
+            });
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
